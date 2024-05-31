825892aa8dc333f71c1c6a7d3cbcf1af0e21c5c3
Marcelo Araujo <codetrsh@gmail.com>
2024-05-31 14:52:57 -0300

    cookie samesite none

--- api/controller/controller.js
+++ api/controller/controller.js
@@ -38,7 +38,7 @@ export async function login (req, res) {
         expires: expirationDate,
         httpOnly: true,
         secure: true,
-        sameSite: 'strict',
+        sameSite: 'none',
         path: '/' 
       })
       res.status(200).json({user, token});
@@ -58,7 +58,7 @@ export async function logout(req, res) {
     expires: new Date(0),
     httpOnly: true,
     secure: true,
-    sameSite: 'strict',
+    sameSite: 'none',
     path: '/' 
   })
   res.status(200).json({ success: true, message: 'Logged out successfully' });
